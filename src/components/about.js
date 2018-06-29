edfebf910945ad45b77ae009d915b82433c8ba4a
Scotty Parlor <sparlor@investcloud.com>
2018-06-29 16:35:42 -0400

    portfolio correcting sizing

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import sp from '../images/profile.png';
 
 const About = () => {
     return (
-    <div className='about'>
+    <div className='about' id='aboutMe'>
         <img id='sparlor' src={sp}/>
         <h1 id='aboutContent'>About Me</h1>
         <p id='aboutContent'>Hi, my name is Scotty Parlor. I am a web and software developer in Tampa, FL. I work professionally with Java and Groovy, and work on the side with ReactJS, Node.js, Ruby on Rails, Django, and Vue.js. I desire to continue growing in my learning and experience as I pursue more opportunities to serve my community.</p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import style from '../styles/navbar.css';
 
-window.onscroll = function() {NavigationScroll()};
-
 const NavigationScroll = () => {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 10) {
         document.getElementsByClassName("navbar").className="navScroll";
     }
 }
 
+window.onscroll = function() {
+    NavigationScroll()
+};
+
 const Navbar = () => {
 
         return(
             <div className="navbar">
-                <a href='#'>Home</a>
-                <a href='#'>About</a>
+                <a href='#aboutMe'>About</a>
                 <a href='#'>Projects</a>
+                <a href='#'>Languages</a>
                 <a href='#'>Contact</a>
             </div>
         );
